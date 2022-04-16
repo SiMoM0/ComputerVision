af52eeb584c5aaa705002f400c518d4b894693c0
SiMoM0 <simomosco99@gmail.com>
2022-04-16 13:22:29 +0200

    Improved code

--- Lab5/task1.cpp
+++ Lab5/task1.cpp
@@ -7,25 +7,6 @@
 using namespace cv;
 using namespace std;
 
-Mat src;
-
-//Mouse callback for region growing
-void mouseCallback(int event, int x, int y, int flags, void* userdata)
-{
-    if  ( event == EVENT_LBUTTONDOWN )
-    {
-        vector<pair<int, int>>* ppoints = static_cast<vector<pair<int, int>>*>(userdata);
-        //Get values of the selected pixel
-        Vec3b pixel = src.at<Vec3b>(y, x);
-        int blue = (int) pixel[0];
-        int green = (int) pixel[1];
-        int red = (int) pixel[2];
-        //Print BGR color for the selected pixel
-        printf("Pixel: (%d, %d), BGR color :(%d, %d, %d)\n", y, x, blue, green, red);
-        ppoints->push_back(pair<int, int>(y, x));
-    }
-}
-
 int main(int argc, char** argv) {
     //check input
     if(argc != 2) {
@@ -33,7 +14,7 @@ int main(int argc, char** argv) {
         return -1;
     }
     //load input image and check it
-    src = imread(argv[1]);
+    Mat src = imread(argv[1]);
     if(src.empty()) {
         printf("Could not open or find image");
         return -1;
@@ -41,18 +22,19 @@ int main(int argc, char** argv) {
     
     //apply otsu threshold
     Mat otsu (src.rows, src.cols, CV_8UC3);
-    otsuSegmentation(src, otsu, 7);
+    otsuSegmentation(src, otsu, 9);
+    //save otsu image with the name otsu_{asphalt number}.png in Images folder
+    string filename_otsu = "Images/otsu_" + string(argv[1]).substr(10);
+    imwrite(filename_otsu, otsu);
 
     vector<pair<int, int>> points;
 
     //show images
     namedWindow("Image");
-    setMouseCallback("Image", mouseCallback, &points); // Register the callback function
     imshow("Image", src);
     namedWindow("Otsu thresholding");
     imshow("Otsu thresholding", otsu);
     waitKey(0);
-
 
     //apply regionGrowing and show image
     Mat region = regionGrowing(src, points, 20);
